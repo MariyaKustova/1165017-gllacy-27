200b52fab196bd7492df880c2f03782c5bcbba14
MariyaKustova <masha.kustova-guselnikova@yandex.ru>
2020-03-22 19:34:52 +0500

    Добавила анимацию формам

--- js/catalog.js
+++ js/catalog.js
@@ -6,12 +6,24 @@ var modalSearch = document.querySelector(".modal-search");
 
 var appearEntrance = document.querySelector(".btn-entrance");
 var modalEntrance = document.querySelector(".modal-entrance");
+var emailEntrance = document.querySelector(".email-entrance");
+var passwordEntrance = document.querySelector(".password-entrance");
 
 var appearBusket = document.querySelector(".btn-busket");
 var modalBusket = document.querySelector(".modal-busket");
 
 var appearSorting = document.querySelector(".btn-sorting");
 var modalSorting = document.querySelector(".modal-sorting");
+
+var isStorageSupport = true;
+var storageEmailEntrance = "";
+
+try {
+    storageEmailEntrance = localStorage.getItem("emailEntrance");
+    emailEntrance.value = storageEmailEntrance;
+      } catch (err) {
+    isStorageSupport = false;
+  }
 
 appearMenu.addEventListener("click", function(evt) {
     evt.preventDefault();
@@ -37,3 +49,20 @@ appearSorting.addEventListener("click", function(evt) {
     evt.preventDefault();
     modalSorting.classList.toggle("appear-modal");
 });
+
+modalEntrance.addEventListener("submit", function(evt) {
+    if (
+      !emailEntrance.value ||
+      !passwordEntrance.value 
+    ) {
+      evt.preventDefault();
+      modalEntrance.classList.add("modal-error");
+      setTimeout(function() {
+          modalEntrance.classList.remove("modal-error");
+      }, 1000);
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("emailEntrance", emailEntrance.value);
+      }
+    }
+  });
--- js/feedback-form.js
+++ js/feedback-form.js
@@ -6,6 +6,8 @@ var modalSearch = document.querySelector(".modal-search");
 
 var appearEntrance = document.querySelector(".btn-entrance");
 var modalEntrance = document.querySelector(".modal-entrance");
+var emailEntrance = document.querySelector(".email-entrance");
+var passwordEntrance = document.querySelector(".password-entrance");
 
 var appearBusket = document.querySelector(".btn-busket");
 var modalBusket = document.querySelector(".modal-busket");
@@ -13,21 +15,40 @@ var modalBusket = document.querySelector(".modal-busket");
 var appearFeedbackForm = document.querySelector(".btn.btn-address");
 var modalFeedbackForm = document.querySelector(".feedback-form");
 var closeFeedbackForm = document.querySelector(".btn-close");
-var feedbackInputName = document.querySelector("[id=name-surname]");
-var feedbackInputEmail = document.querySelector("[id=email-feedback-form]");
-var feedback = document.querySelector("[id=feedback]");
-var submitFeedback = document.querySelector(".feedback-form .btn-form");
+var feedbackInputName = document.getElementById("name-surname");
+var feedbackInputEmail = document.getElementById("email-feedback-form");
+var feedback = document.getElementById("feedback");
+
+var mailingForm = document.querySelector(".newsletter-subscription");
+var inputEmailMailing = document.querySelector(".input-email-mailing")
 
 var isStorageSupport = true;
 var storageFeedbackInputName = "";
 var storageFeedbackInputEmail = "";
+var storageEmailEntrance = "";
+var storageInputEmailMailing = "";
+
 
 try {
-    storageFeedbackInputName = localStorage.getItem("feedbackInputName");
-    feedbackInputName.value = storageFeedbackInputName;
-    storageFeedbackInputEmail = localStorage.getItem("feedbackInputEmail");
-    feedbackInputEmail.value = storageFeedbackInputEmail;
-  } catch (err) {
+  storageFeedbackInputName = localStorage.getItem("feedbackInputName");
+  feedbackInputName.value = storageFeedbackInputName;
+  storageFeedbackInputEmail = localStorage.getItem("feedbackInputEmail");
+  feedbackInputEmail.value = storageFeedbackInputEmail;
+} catch (err) {
+  isStorageSupport = false;
+}
+
+try {
+    storageEmailEntrance = localStorage.getItem("emailEntrance");
+    emailEntrance.value = storageEmailEntrance;
+      } catch (err) {
+    isStorageSupport = false;
+  }
+
+  try {
+    storageInputEmailMailing = localStorage.getItem("inputEmailMailing");
+    inputEmailMailing.value = storageInputEmailMailing;
+      } catch (err) {
     isStorageSupport = false;
   }
 
@@ -68,21 +89,54 @@ closeFeedbackForm.addEventListener("keydown", function(evt) {
   }
 });
 
-submitFeedback.addEventListener("submit", function(evt) {
+modalEntrance.addEventListener("submit", function(evt) {
   if (
-    !feedbackInputName.value ||
-    !feedbackInputEmail.value ||
-    !feedback.value
+    !emailEntrance.value ||
+    !passwordEntrance.value 
   ) {
     evt.preventDefault();
-    modalFeedbackForm.classList.add("modal-error");
+    modalEntrance.classList.add("modal-error");
     setTimeout(function() {
-      modalFeedbackForm.classList.remove("modal-error");
+        modalEntrance.classList.remove("modal-error");
     }, 1000);
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("feedbackInputName", feedbackInputName.value);
-      localStorage.setItem("feedbackInputEmail", feedbackInputEmail.value);
+      localStorage.setItem("emailEntrance", emailEntrance.value);
     }
   }
 });
+
+mailingForm.addEventListener("submit", function(evt) {
+    if (
+      !inputEmailMailing.value 
+    ) {
+      evt.preventDefault();
+      mailingForm.classList.add("modal-error");
+      setTimeout(function() {
+        mailingForm.classList.remove("modal-error");
+      }, 1000);
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("inputEmailMailing", inputEmailMailing.value);
+      }
+    }
+  });
+
+modalFeedbackForm.addEventListener("submit", function(evt) {
+    if (
+      !feedbackInputName.value ||
+      !feedbackInputEmail.value ||
+      !feedback.value
+    ) {
+      evt.preventDefault();
+      modalFeedbackForm.classList.add("modal-error");
+      setTimeout(function() {
+        modalFeedbackForm.classList.remove("modal-error");
+      }, 1000);
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("feedbackInputName", feedbackInputName.value);
+        localStorage.setItem("feedbackInputEmail", feedbackInputEmail.value);
+      }
+    }
+  });
